fecae22fcac8929374da4bfef310d9b170095b75
manishboro <officialmanishboro@gmail.com>
2020-03-09 20:33:11 +0530

    version 2.0

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-var code = document.getElementById("bg-value--code");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-var direction = document.querySelector(".select");
-var copyText = document.getElementById("bg-code");
-var button = document.querySelector(".button");
+let code = document.getElementById("linear-gradient-code--input");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let setBackgroundColor = document.getElementById("gradient");
+let direction = document.querySelector(".select-direction--options");
+let button = document.querySelector(".button");
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient("+ direction.value +", " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")" ;
+  setBackgroundColor.style.background =
+    "linear-gradient(" +
+    direction.value +
+    ", " +
+    color1.value +
+    ", " +
+    color2.value +
+    ")";
 
-	code.value = body.style.background + ";";
+  code.value = setBackgroundColor.style.background + ";";
 }
 
 function copyTextFunc() {
-	copyText = document.getElementById("bg-value--code");
+  /* Select the text field */
+  code.select();
+  code.setSelectionRange(0, 99999); /*For mobile devices*/
 
-	/* Select the text field */
-	copyText.select();
-	copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-  
-	/* Copy the text inside the text field */
-	document.execCommand("copy");
-  
-	/* Alert the copied text */
-	alert("Copied to clip-board : " + copyText.value);
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
 }
 
 color1.addEventListener("input", setGradient);
@@ -38,4 +34,3 @@ color2.addEventListener("input", setGradient);
 direction.addEventListener("click", setGradient);
 
 button.addEventListener("click", copyTextFunc);
-
